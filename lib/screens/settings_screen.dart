2240d83913dd90a2ad4e10f2ed3cedd18305b156
apple <apple@192.168.1.15>
2026-06-19 19:51:12 +0800

    fix: 更正开源协议为AGPL-3.0
    - 设置页面显示从MIT改为AGPL-3.0
    - 协议对话框内容更新为AGPL-3.0
    - 新增LICENSE文件

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -799,7 +799,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ListTile(
             leading: const Icon(Icons.code, color: WFColors.info),
             title: Text(l10n.openSourceLicense),
-            subtitle: const Text('MIT License'),
+            subtitle: const Text('AGPL-3.0'),
             trailing: const Icon(Icons.open_in_new, size: 18),
             onTap: () => _showLicenseDialog(),
           ),
@@ -841,26 +841,23 @@ class _SettingsScreenState extends State<SettingsScreen> {
   void _showLicenseDialog() {
     WFDialog.show(
       context,
-      title: 'MIT License',
+      title: 'AGPL-3.0',
       content: const SingleChildScrollView(
         child: Text(
-          'MIT License\n\n'
+          'AGPL-3.0 License\n\n'
           'Copyright (c) 2024 WriteFont\n\n'
-          'Permission is hereby granted, free of charge, to any person obtaining a copy '
-          'of this software and associated documentation files (the "Software"), to deal '
-          'in the Software without restriction, including without limitation the rights '
-          'to use, copy, modify, merge, publish, distribute, sublicense, and/or sell '
-          'copies of the Software, and to permit persons to whom the Software is '
-          'furnished to do so, subject to the following conditions:\n\n'
-          'The above copyright notice and this permission notice shall be included in all '
-          'copies or substantial portions of the Software.\n\n'
-          'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR '
-          'IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, '
-          'FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE '
-          'AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER '
-          'LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, '
-          'OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE '
-          'SOFTWARE.',
+          'This program is free software: you can redistribute it and/or modify '
+          'it under the terms of the GNU Affero General Public License as published by '
+          'the Free Software Foundation, either version 3 of the License, or '
+          '(at your option) any later version.\n\n'
+          'This program is distributed in the hope that it will be useful, '
+          'but WITHOUT ANY WARRANTY; without even the implied warranty of '
+          'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the '
+          'GNU Affero General Public License for more details.\n\n'
+          'You should have received a copy of the GNU Affero General Public License '
+          'along with this program. If not, see https://www.gnu.org/licenses/.\n\n'
+          'Note: The InkForge core engine is proprietary and not covered by this license. '
+          'For source code access, please contact the author.',
           style: TextStyle(fontSize: 13, height: 1.5),
         ),
       ),
